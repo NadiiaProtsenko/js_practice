72dfcb604c77bb68df346ccf4b8468a31f9ebd7b
Nadiia <philimonovanadiya@gmail.com>
2024-03-01 14:21:10 +0200

    add task2.js

---
+++ js_basic_task2/task2.js
@@ -0,0 +1,55 @@
+        let basePrice = 10000;
+        let year = 2020;
+        let mileage = 75000;
+        let hasClimateControl = true;
+        let safetyPackage = true;
+        let isElectric = false;
+        let addyear;
+        let addmileage;
+        let addhasClimateControl;
+        let addsafetyPackage;
+        let addisElectric;
+
+
+        if (year >= 2019 && year <= 2022) {
+            addyear = basePrice*10/100;
+        }
+        else {
+            addyear = 0;
+        }
+
+        if (mileage >= 50000 && mileage <= 100000) {
+            addmileage = -(basePrice*5/100);
+        }
+        else {
+            addmileage = 0;
+        }
+
+        if (hasClimateControl) {
+            addhasClimateControl = basePrice*7/100;
+        }
+        else {
+            addhasClimateControl = 0;
+        }
+
+        if (safetyPackage) {
+            addsafetyPackage = basePrice*8/100;
+        }
+        else {
+            addsafetyPackage = 0;
+        }
+
+        if (isElectric) {
+            addisElectric = basePrice*15/100;
+        }
+        else {
+            addisElectric = 0;
+        }
+
+        let finalPrice;
+        finalPrice = basePrice + addyear+addmileage+addhasClimateControl+addsafetyPackage+addisElectric;
+
+        console.log("Base price was " + basePrice + ", and final price is " + finalPrice+ ".");
+        console.log("Base price was " + basePrice + ", and year added " + addyear +", and mileage added " + addmileage + ", and hasClimateControle added " + addhasClimateControl + ", and safety package added " + addsafetyPackage + ", and isElectric added " +addisElectric + ".");
+
+    
